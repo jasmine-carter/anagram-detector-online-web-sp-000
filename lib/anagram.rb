6b32a8bfd71f6f2735a8ae7eb0a1120062600579
Jasmine Carter <jasmine.carter87@gmail.com>
2019-07-25 00:26:36 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,7 +9,7 @@ attr_accessor :word
   def match(anagrams)#should return all matches of anagrams against self in an array
     matches = []
     anagrams.each do |gram|
-      if gram == @word
+      if gram.split("").sort == @word.split("").sort
         matches << gram
       end
     end
